cc132cf610a42304623f5b671f0e2cda2aa751b0
tsims115 <tsims115@yahoo.com>
2022-03-24 17:48:10 -0700

    making it import only the todos I need and fixing import statements

--- 0x0E-api/0-gather_data_from_an_API.py
+++ 0x0E-api/0-gather_data_from_an_API.py
@@ -2,9 +2,9 @@
 """Module 0-gather_data_from_an_API GETS information from url and with id"""
 
 
-from urllib import request
-import sys
 import json
+import sys
+from urllib import request
 
 
 if __name__ == '__main__':
@@ -13,17 +13,18 @@ if __name__ == '__main__':
     uid = sys.argv[1]
     with request.urlopen("{}/{}".format(uurl, uid)) as response:
         user = json.loads(response.read().decode('utf8'))
-    with request.urlopen(turl) as response:
+    with request.urlopen("{}/?userId={}".format(turl, uid)) as response:
         todos = json.loads(response.read().decode('utf8'))
     employee_name = user['name']
     num_done = 0
     tasks_done = []
-    start_pos = 0 + ((int(uid) - 1) * 20)
-    for task in todos[start_pos:start_pos+20]:
+    tot_tasks = 0
+    for task in todos:
         if task['completed']:
             num_done += 1
             tasks_done.append(task['title'])
+        tot_tasks += 1
     print("Employee {} is done with tasks".format(employee_name), end="")
-    print("({}/20):".format(num_done))
+    print("({}/{}):".format(num_done, tot_tasks))
     for title in tasks_done:
         print("\t {}".format(title))
