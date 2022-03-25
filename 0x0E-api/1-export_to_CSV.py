8e21f7a422ecee42312c9c4f81c34d385099bff6
tsims115 <tsims115@yahoo.com>
2022-03-24 18:38:08 -0700

    Adding task 0 stuff

--- 0x0E-api/1-export_to_CSV.py
+++ 0x0E-api/1-export_to_CSV.py
@@ -16,11 +16,21 @@ if __name__ == '__main__':
         user = json.loads(response.read().decode('utf8'))
     with request.urlopen("{}/?userId={}".format(turl, uid)) as response:
         todos = json.loads(response.read().decode('utf8'))
+    name = user['name']
     username = user['username']
-    rows = []
+    rows, tasks_done = [], []
+    tot_tasks, num_done = 0, 0
     for task in todos:
+        if task['completed']:
+            num_done += 1
+            tasks_done.append(task['title'])
+        tot_tasks += 1
         row = [uid, username, str(task['completed']), task['title']]
         rows.append(row)
+    print("Employee {} is done with tasks".format(name), end="")
+    print("({}/{}):".format(num_done, tot_tasks))
+    for title in tasks_done:
+        print("\t {}".format(title))
     filename = '{}.csv'.format(uid)
     with open(filename, 'w+', encoding='UTF8', newline='') as f:
         writer = csv.writer(f)
