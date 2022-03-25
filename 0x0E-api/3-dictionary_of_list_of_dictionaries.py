339eab955eb1781e505e8ca0119c888c76c0b239
tsims115 <tsims115@yahoo.com>
2022-03-24 21:04:40 -0700

    Adding task 3

---
+++ 0x0E-api/3-dictionary_of_list_of_dictionaries.py
@@ -0,0 +1,33 @@
+#!/usr/bin/python3
+"""Module 0-gather_data_from_an_API GETS information from url and with id"""
+
+
+import json
+import sys
+from urllib import request
+import requests
+
+
+if __name__ == '__main__':
+    turl = 'https://jsonplaceholder.typicode.com/todos'
+    uurl = 'https://jsonplaceholder.typicode.com/users'
+    data = {}
+    with request.urlopen("{}".format(uurl)) as response:
+        users = json.loads(response.read().decode('utf8'))
+    for user in users:
+        uid = user['id']
+        todos = requests.get("{}/?userId={}".format(turl, uid)).json()
+        json_list = []
+        for task in todos:
+            json_task = {
+                "username": user['name'],
+                "task": task['title'],
+                "completed": task['completed']
+            }
+            json_list.append(json_task)
+        data[str(uid)] = json_list
+    filename = "todo_all_employees.json"
+    with open(filename, 'w+', encoding='UTF8') as f:
+        json.dump(data, f)
+    """with request.urlopen("{}/?userId={}".format(turl, uid)) as response:
+        todos = json.loads(response.read().decode('utf8'))"""
